a22dc84853519b9011547acc5aa388e2847c4ca0
dungcodejob <dungcodejob@gmail.com>
2025-05-16 10:56:25 +0700

    feat(storybook): add @storybook/addon-interactions dependency
    This commit adds the @storybook/addon-interactions package to the project dependencies and enables it in the Storybook configuration. This addition enhances the testing capabilities within Storybook by allowing interaction testing for components.

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -23,7 +23,7 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
-    // '@storybook/addon-interactions',
+    '@storybook/addon-interactions',
   ],
   framework: {
     name: getAbsolutePath('storybook-react-rsbuild'),
